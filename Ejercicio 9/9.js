6ee9c4afcfc6b1e33a7476333b1f53fe5fe78d75
Leandro-Aballone <l.aballone@gmail.com>
2023-05-13 00:31:52 -0300

    Ejercicio 9

---
+++ Ejercicio 9/9.js
@@ -0,0 +1,9 @@
+/* Ejercicio 9
+Declarar una variable de nombre miGustoDePizza y asignarle la variedad de pizza que más te guste 
+usando un String (cadena de caracteres). Mostrar el valor de la variable por la consola. */
+
+let miGustoDePizza = "Provolone";
+console.log("La Pizza que más me gusta es de: " + miGustoDePizza);
+
+
+/* El que lee invita. */
