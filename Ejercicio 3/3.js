83d4184c88bfde9f5d5c306da73afaf3d413dfec
Leandro-Aballone <l.aballone@gmail.com>
2023-05-12 23:57:09 -0300

    Ejercicio 3

---
+++ Ejercicio 3/3.js
@@ -0,0 +1,7 @@
+/* Ejercicio 3
+Escribe un programa que le pregunte al usuario su nombre e imprima "Hola " seguido del nombre y un 
+signo de exclamación.
+ */
+
+let nombre = prompt("Ingrese su nombre: ");
+console.log("Hola " + nombre + "!");
