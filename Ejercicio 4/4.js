33cb9981668517c3293672e761aa5b5c40e3c912
Leandro-Aballone <l.aballone@gmail.com>
2023-05-13 00:01:08 -0300

    Ejercicio 4

---
+++ Ejercicio 4/4.js
@@ -0,0 +1,13 @@
+/* Ejercicio 4
+Escribe un programa que le pida al usuario su nombre y apellido y lo imprima por consola
+ */
+
+/* ALTERNATIVA 1 */
+/* let nombre_y_apellido = prompt("Ingrese Nombre y Apellido: ");
+console.log(nombre_y_apellido);  */
+
+/* ALTERNATIVA 2 */
+let nombre = prompt("Ingresar Nombre: ");
+let apellido = prompt("Ingresar Apellido: ");
+
+console.log(nombre + " " + apellido);
