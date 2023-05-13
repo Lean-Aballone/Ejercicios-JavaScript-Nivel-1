8551a7b24de85d7d9144ae401fc39b2314de6b50
Leandro-Aballone <l.aballone@gmail.com>
2023-05-13 00:24:03 -0300

    Ejercicio 7

---
+++ Ejercicio 7/7.js
@@ -0,0 +1,6 @@
+/* Ejercicio 7
+Declarar una variable de nombre cantidadDeVentanas y asignarle la cantidad de ventanas que hay en 
+tu casa. Mostrar el valor de la variable por la consola. */
+
+let cantidadDeVentanas = prompt("Ingresar Cantidad de ventanas: ");
+console.log("La cantidad de ventanas es: " + cantidadDeVentanas);
