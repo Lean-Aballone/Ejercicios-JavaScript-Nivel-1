631a82f73f175d40d9bc139634bb4b7a939882a1
Leandro-Aballone <l.aballone@gmail.com>
2023-05-13 00:35:39 -0300

    Ejercicio 10

---
+++ Ejercicio 10/10.js
@@ -0,0 +1,9 @@
+/* Ejercicio 10
+Mostrar por la consola los siguientes valores false, 'Hola', 0 y 'Minions' usando la misma variable. */
+
+let variable = false;
+console.log(variable);
+variable = "'Hola'";
+console.log(variable);
+variable = "'Minions'";
+console.log(variable);
