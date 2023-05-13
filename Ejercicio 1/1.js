c076ddc0318c84e71023d3a9fc0e29d0d8c04dd5
Leandro-Aballone <l.aballone@gmail.com>
2023-05-12 23:52:09 -0300

    Ejercicio 1

---
+++ Ejercicio 1/1.js
@@ -0,0 +1,11 @@
+/* Ejercicio 1
+Mostrar por consola, tu nombre, apellido, comisión y hobbies
+ */
+let nombre = "Leandro";
+let apellido = "Aballone";
+let comision = 23063;
+let hobbies = "Programar";
+console.log("Nombre: " + nombre);
+console.log("Apellido: " + apellido);
+console.log("Comisión: " + comision);
+console.log("Hobbies: " + hobbies);
